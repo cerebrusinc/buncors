db27ab97817edb33b15e0acc59c426dba2b08746
Lewis Mosho Jr <lmosho@techtronicsltd.com>
2023-10-27 04:24:14 +0200

    init

---
+++ README.md
@@ -0,0 +1,96 @@
+<p align="center">
+    <img src="https://drive.google.com/uc?id=1N3CW-z4Ekm69tR6sbcpw_InyUGnxz3wb" alt="buncors logo" width="250" height="250" />
+</p>
+
+# buncors
+
+The cors middleware that enables a [bunrest](https://www.npmjs.com/package/bunrest) server to handle cors requests. It also handles preflight requests 😃.
+
+This version depends on **bunrest** `^1.3.6`
+
+## Default Response Headers
+
+If no options are provided, the response headers will be as follows:
+
+```txt
+Access-Control-Allow-Origin: *
+Access-Control-Allow-Methods: GET,HEAD,PUT,PATCH,POST,DELETE
+Access-Control-Allow-Headers: Content-Type
+Access-Control-Max-Age: 5
+```
+
+**NOTE:** The allow headers will always append `Content-Type` to your response headers so no need to add it to the list.
+
+## Usage Example
+
+```ts
+import server from "bunrest";
+const app = server();
+
+app.post("/auth", cors(), async (req, res) => {
+	// some processing code
+	res.status(200).json({ success: true });
+});
+
+app.listen(Bun.env.PORT, () => {
+	console.log(`[startup]: Server running on port "${Bun.env.PORT}"`);
+});
+```
+
+## Preflight Example
+
+```ts
+import server from "bunrest";
+const app = server();
+
+app.post("/auth", async (req, res) => {
+	// some processing code
+	res.status(200).json({ success: true });
+});
+
+app.options(
+	"/auth",
+	cors({
+		allowedHeaders: ["X-TOKEN"],
+		methods: ["POST"],
+		origins: ["https://www.cerebrus.dev"],
+	})
+);
+
+app.listen(Bun.env.PORT, () => {
+	console.log(`[startup]: Server running on port "${Bun.env.PORT}"`);
+});
+```
+
+## CorsOptions Interface
+
+```ts
+	origins?: string | string[];
+	methods?: string[];
+	allowedHeaders?: string[];
+	maxAge?: number;
+	allowCredentials?: boolean;
+	exposedHeaders?: string[];
+```
+
+| Param            | Type                          | Default                          | Is Required? | Description                                                                                                                               |
+| ---------------- | ----------------------------- | -------------------------------- | ------------ | ----------------------------------------------------------------------------------------------------------------------------------------- |
+| origins          | `string, string[], undefined` | `*`                              | No           | Sets the `Access-Control-Allow-Origin` header; if set, it will dynamically return the correct origin or the first origin is not accetped. |
+| methods          | `string[], undefined`         | `GET,HEAD,PUT,PATCH,POST,DELETE` | No           | Sets the `Access-Control-Allow-Methods` header.                                                                                           |
+| allowedHeaders   | `string[], undefined`         | `Content-Type`                   | No           | Sets the `Access-Control-Allow-Headers` header; will always append `Content-Type` to the allowed headers.                                 |
+| maxAge           | `number, undefined`           | `5`                              | No           | Sets the `Access-Control-Max-Age` header in **seconds**.                                                                                  |
+| allowCredentials | `boolean, undefined`          | `undefined`                      | No           | Sets the `Access-Control-Allow-Credentials` header.                                                                                       |
+| exposedHeaders   | `string[], undefined`         | `undefined`                      | No           | Sets the `Access-Control-Expose-Headers` header.                                                                                          |
+
+<br />
+
+# Changelog
+
+## v0.1.x
+
+<details open>
+<summary><strong>v0.1.0</strong></summary>
+
+- Initial commit
+</details>
+<br />
---
+++ src/index.ts
@@ -0,0 +1,229 @@
+import { Handler } from "bunrest/src/server/request";
+
+/**
+ * ```ts
+ * import type { CorsOptions } from "buncors";
+ * ```
+ * The cors options interface enabling you to take full control of your bunrest server's security! If not set, the default headers set will be:
+ *
+ * ```txt
+ * Access-Control-Allow-Origin: *
+ * Access-Control-Allow-Methods: GET,HEAD,PUT,PATCH,POST,DELETE
+ * Access-Control-Allow-Headers: Content-Type
+ * Access-Control-Max-Age: 5
+ * ```
+ */
+interface CorsOptions {
+	/**Sets the `Access-Control-Allow-Origin` header; Defaults to `*` but if set, it will dynamically return the correct origin or the first origin is not accetped  */
+	origins?: string | string[];
+	/**Sets the `Access-Control-Allow-Methods` header; Defaults to `GET,HEAD,PUT,PATCH,POST,DELETE` */
+	methods?: string[];
+	/**Sets the `Access-Control-Allow-Headers` header; Defaults to `Content-Type` and will always append `Content-Type` to the allowed headers */
+	allowedHeaders?: string[];
+	/**Sets the `Access-Control-Max-Age` header in **seconds**; Defaults to `5` */
+	maxAge?: number;
+	/**Sets the `Access-Control-Allow-Credentials` header; This header is **NOT SET** unless you give it a value */
+	allowCredentials?: boolean;
+	/**Sets the `Access-Control-Expose-Headers` header; This header is **NOT SET** unless you give it a value */
+	exposedHeaders?: string[];
+}
+
+/**
+ * The cors middleware that enables a `bunrest` server to handle cors requests. It also handles preflight requests :)
+ *
+ * ### Default Response Headers
+ * If no options are provided, the response headers will be as follows:
+ *
+ * ```txt
+ * Access-Control-Allow-Origin: *
+ * Access-Control-Allow-Methods: GET,HEAD,PUT,PATCH,POST,DELETE
+ * Access-Control-Allow-Headers: Content-Type
+ * Access-Control-Max-Age: 5
+ * ```
+ *
+ * **NOTE:** The allow headers will always append `Content-Type` to your response headers so no need to add it to the list
+ *
+ * ### Usage Example
+ *
+ * ```ts
+ * import server from "bunrest";
+ * const app = server();
+ *
+ * app.post("/auth", cors(), async (req, res) => {
+ *      // some processing code
+ *      res.status(200).json({ success: true });
+ * });
+ *
+ * app.listen(Bun.env.PORT, () => {
+ *      console.log(`[startup]: Server running on port "${Bun.env.PORT}"`);
+ * });
+ * ```
+ *
+ * ### Preflight Example
+ *
+ * ```ts
+ * import server from "bunrest";
+ * const app = server();
+ *
+ * app.post("/auth", async (req, res) => {
+ *      // some processing code
+ *      res.status(200).json({ success: true });
+ * });
+ *
+ * app.options("/auth", cors({
+ *      allowedHeaders: ["X-TOKEN"],
+ *      methods: ["POST"],
+ *      origins: ["https://www.cerebrus.dev"]
+ * }));
+ *
+ * app.listen(Bun.env.PORT, () => {
+ *      console.log(`[startup]: Server running on port "${Bun.env.PORT}"`);
+ * });
+ * ```
+ *
+ * @param options `CorsOptions`; Defaults to undefined with presets.
+ * @returns a middleware function; type `Handler`
+ * @version bunrest : ^1.3.6
+ */
+function cors(options?: CorsOptions): Handler {
+	const origins: string | string[] = options?.origins ? options.origins : "*";
+	const methods: string[] = options?.methods
+		? options.methods
+		: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"];
+	const allowedHeaders: string[] = options?.allowedHeaders
+		? [...options.allowedHeaders, "Content-Type"]
+		: ["Content-Type"];
+	const maxAge: number = options?.maxAge ? options.maxAge : 5;
+	const allowCredentials: boolean | undefined = options?.allowCredentials
+		? options.allowCredentials
+		: undefined;
+	const exposedHeaders: string[] | undefined = options?.exposedHeaders
+		? options.exposedHeaders
+		: undefined;
+
+	const _isOrigin = (header: string): boolean => {
+		if (Array.isArray(origins)) return origins.includes(header);
+		else return true;
+	};
+
+	const _isValidMethod = (method: string): boolean => {
+		return methods.includes(method);
+	};
+
+	const _isValidRequestHeaders = (headers: string): boolean => {
+		return allowedHeaders.join(",").toLowerCase().includes(headers);
+	};
+
+	const middelware: Handler = (req, res, next?, err?) => {
+		switch (req.method) {
+			case "OPTIONS":
+				// Get the origin header and compare it's value to the initialised value
+				const origin = req.headers ? req.headers["origin"] : "*";
+				const isvalidOrigin = _isOrigin(origin);
+
+				// Get the request method header (if none, then use the request mehtod) and compare it's value to the initialised value
+				const method = req.headers
+					? req.headers["access-control-request-method"]
+					: req.method;
+				const isValidMethod = _isValidMethod(method);
+
+				// In case of a preflight, check the request headers and compare them to the allowed headers
+				const requestHeaders = req.headers
+					? req.headers["access-control-request-headers"]
+					: null;
+				const isValidHeaders = requestHeaders
+					? _isValidRequestHeaders(requestHeaders)
+					: true;
+
+				if (isvalidOrigin && isValidMethod && isValidHeaders) {
+					const resHeaders: { [key: string]: any } = {};
+					switch (Array.isArray(origins)) {
+						case true:
+							resHeaders["Access-Control-Allow-Origin"] = origin;
+							res.setHeader("Access-Control-Allow-Origin", origin);
+
+							resHeaders["Vary"] = "Accept-Encoding, Origin";
+							res.setHeader("Vary", "Accept-Encoding, Origin");
+
+							break;
+						default:
+							resHeaders["Access-Control-Allow-Origin"] = "*";
+							res.setHeader("Access-Control-Allow-Origin", "*");
+					}
+
+					resHeaders["Access-Control-Allow-Methods"] = methods.join(",");
+					res.setHeader("Access-Control-Allow-Methods", methods.join(","));
+
+					resHeaders["Access-Control-Allow-Headers"] = allowedHeaders.join(",");
+					res.setHeader(
+						"Access-Control-Allow-Headers",
+						allowedHeaders.join(",")
+					);
+
+					resHeaders["Access-Control-Max-Age"] = maxAge;
+					res.setHeader("Access-Control-Max-Age", maxAge);
+
+					if (allowCredentials) {
+						resHeaders[
+							"Access-Control-Allow-Credentials"
+						] = `${allowCredentials}`;
+						res.setHeader(
+							"Access-Control-Allow-Credentials",
+							`${allowCredentials}`
+						);
+					}
+
+					if (exposedHeaders) {
+						resHeaders["Access-Control-Expose-Headers"] =
+							exposedHeaders.join(",");
+						res.setHeader(
+							"Access-Control-Expose-Headers",
+							exposedHeaders.join(",")
+						);
+					}
+
+					res
+						.option({
+							status: 204,
+							headers: resHeaders,
+							statusText: "204 No Content",
+						})
+						.headers(resHeaders)
+						.status(204)
+						.send("");
+				} else if (!isvalidOrigin) {
+					res
+						.option({ status: 400, statusText: "400 Bad Request" })
+						.status(400)
+						.send("");
+				} else if (!isValidMethod) {
+					res
+						.option({ status: 405, statusText: "405 Method Not Allowed" })
+						.status(405)
+						.send("");
+				} else {
+					res
+						.option({ status: 406, statusText: "406 Not Acceptable" })
+						.status(406)
+						.send("");
+				}
+				console.log("\nheaders\n", res.getHeader(), "\n");
+				break;
+			default:
+				// @ts-ignore
+				const host = req.headers["host"];
+				const isvalidOriginNoPreflight = _isOrigin(host);
+
+				if (!isvalidOriginNoPreflight) {
+					res.status(400).send("");
+				}
+				// @ts-ignore
+				next();
+		}
+	};
+
+	return middelware;
+}
+
+export default cors;
+export { CorsOptions };
